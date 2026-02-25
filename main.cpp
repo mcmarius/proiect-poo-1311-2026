1b291778c3cacf09aef4485ac00df8a2436a211d
mcmarius <23401453+mcmarius@users.noreply.github.com>
2026-02-25 12:02:54 +0200

    Laborator 1 tema 1 pe lung

--- main.cpp
+++ main.cpp
@@ -7,13 +7,205 @@
 // This also works if you do not want `include/`, but some editors might not like it
 // #include "Example.h"
 
+class PanouIluminare {
+    bool aprins;
+    int brightness;
+    std::string firma;
+
+public:
+    PanouIluminare(bool aprins, int brightness, const std::string &firma)
+        : aprins(aprins),
+          brightness(brightness),
+          firma(firma) {
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const PanouIluminare &obj) {
+        return os
+               << "aprins: " << obj.aprins
+               << " brightness: " << obj.brightness
+               << " firma: " << obj.firma;
+    }
+};
+
+class GrupIluminare {
+    std::vector<PanouIluminare> panouri;
+
+public:
+    explicit GrupIluminare(const std::vector<PanouIluminare> &panouri)
+        : panouri(panouri) {
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const GrupIluminare &grup) {
+        os << "panouri:\n";
+        for (const PanouIluminare &panou: grup.panouri) {
+            os << panou << "\n";
+        }
+        return os;
+    }
+};
+
+class Cuier {
+    int m_nr_haine;
+    int m_nr_randuri;
+
+public:
+    Cuier(int nr_haine, int nr_randuri) : m_nr_haine(nr_haine), m_nr_randuri(nr_randuri) {
+        std::cout << "Cuier nr_haine: " << nr_haine << "\n";
+    }
+
+    ~Cuier() {
+        std::cout << "~Cuier nr_randuri: " << m_nr_randuri << "\n";
+    }
+
+    Cuier(const Cuier &originalu) : m_nr_haine(originalu.m_nr_haine), m_nr_randuri(originalu.m_nr_randuri) {
+        std::cout << "Constr de copiere cuier " << originalu.m_nr_haine << "\n";
+    }
+
+    Cuier &operator=(const Cuier &originalu) {
+        if (this == &originalu) {
+            return *this;
+        }
+        m_nr_haine = originalu.m_nr_haine;
+        m_nr_randuri = originalu.m_nr_randuri;
+        std::cout << "op= cuier\n";
+        return *this;
+    }
+
+    [[nodiscard]] int get_nr_haine() const {
+        return m_nr_haine;
+    }
+
+    void set_nr_haine(int nr_haine) {
+        this->m_nr_haine = nr_haine;
+    }
+
+    int get_nr_randuri() const {
+        return m_nr_randuri;
+    }
+
+    void set_nr_randuri(int nr_randuri) {
+        this->m_nr_randuri = nr_randuri;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Cuier &cuier) {
+        os << cuier.m_nr_haine << " " << cuier.m_nr_randuri;
+        return os;
+    }
+};
+
+
+class Sala {
+    std::vector<GrupIluminare> grupLumina;
+    int numar;
+    double zgomot;
+    Cuier m_cuier;
+
+public:
+    friend std::ostream &operator<<(std::ostream &os, const Sala &sala) {
+        os << "grupLumina:\n";
+        for (const auto &corp: sala.grupLumina) {
+            os << corp << "\n";
+        }
+        os
+                << " numar: " << sala.numar
+                << " zgomot: " << sala.zgomot
+                << " m_cuier: " << sala.m_cuier;
+        return os;
+    }
+
+    Sala(const Cuier &cuier) : m_cuier(cuier) {
+    }
+
+    Sala(const std::vector<GrupIluminare> &grup_lumina, const Cuier &m_cuier)
+        : grupLumina(grup_lumina),
+          m_cuier(m_cuier) {
+    }
+
+    Sala(const Sala &other)
+        : grupLumina(other.grupLumina),
+          numar(other.numar),
+          zgomot(other.zgomot),
+          m_cuier(other.m_cuier) {
+    }
+
+    Sala &operator=(const Sala &other) {
+        if (this == &other)
+            return *this;
+        grupLumina = other.grupLumina;
+        numar = other.numar;
+        zgomot = other.zgomot;
+        m_cuier = other.m_cuier;
+        return *this;
+    }
+
+    Sala(const std::vector<GrupIluminare> &grup_lumina, int numar, double zgomot, const Cuier &m_cuier)
+        : grupLumina(grup_lumina),
+          numar(numar),
+          zgomot(zgomot),
+          m_cuier(m_cuier) {
+    }
+
+    [[nodiscard]] std::vector<GrupIluminare> get_grup_lumina() const {
+        return grupLumina;
+    }
+
+    void set_grup_lumina(const std::vector<GrupIluminare> &grup_lumina) {
+        grupLumina = grup_lumina;
+    }
+
+    [[nodiscard]] int get_numar() const {
+        return numar;
+    }
+
+    void set_numar(int numar) {
+        this->numar = numar;
+    }
+
+    [[nodiscard]] double get_zgomot() const {
+        return zgomot;
+    }
+
+    void set_zgomot(double zgomot) {
+        this->zgomot = zgomot;
+    }
+
+    [[nodiscard]] Cuier get_cuier() const {
+        return m_cuier;
+    }
+
+    void set_cuier(const Cuier &cuier) {
+        this->m_cuier = cuier;
+    }
+};
+
+
 int main() {
-    std::cout << "Hello, 1311 la 8 dimineata din terminal!!\n";
+    Cuier c1{2, 2}, c2{4, 3}, c4 = c2;
+    std::cout << c1 << c2;
+    // c4 = c2;
+    // c4.operator=(c1);
+    std::cout << "inainte de c3\n"; {
+        std::cout << "inainte de c3 in bloc\n";
+        Cuier c3{1, 1};
+        std::cout << "dupa c3 in bloc\n";
+    }
+    std::cout << "dupa c3\n";
+    Sala sala109{c1};
+    Sala s107{{}, 2, 3, c1};
+    sala109.set_grup_lumina({});
+    sala109.set_numar(109);
+    // sala109.set_zgomot(50);
+    Sala sala106{c2};
+    c1 = c2;
+    sala106.set_grup_lumina({});
+    sala106.set_numar(106);
+    sala106.set_zgomot(70);
+    std::cout << c1.get_nr_haine();
+
+    std::cout << "\nHello, 1311 la 8 dimineata din terminal!!\n";
     Example e1;
     e1.g();
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
+
     /////////////////////////////////////////////////////////////////////////
     /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
     /// dați exemple de date de intrare folosind fișierul tastatura.txt
@@ -34,17 +226,7 @@ int main() {
     /// program care merg (și să le evitați pe cele care nu merg).
     ///
     /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
+
     ///////////////////////////////////////////////////////////////////////////
     /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
     /// alt fișier propriu cu ce alt nume doriți.
